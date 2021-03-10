41e05872d7584932f27b18f67589d6985850d282
unknown <you@example.com>
2021-03-11 01:26:09 +0300

    Scroll and burger update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,26 @@
+// Burger
 $(document).ready(function() {
   $('.header__burger').click(function(event) {
     $('.header__burger, .header__menu').toggleClass('active');
     $('body').toggleClass('lock');
   })
 });
+
+// Scroll top
+mybutton = document.getElementById("scrollBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 125 || document.documentElement.scrollTop > 125) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// Cross-browser scroll
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
